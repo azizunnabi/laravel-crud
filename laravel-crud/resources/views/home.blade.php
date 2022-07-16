321a0c058419db6f8d67d7ad8c6be5c0c3c2b3e4
azizunnabi <84537335+azizunnabi@users.noreply.github.com>
2022-07-17 00:34:20 +0500

    laravel transferred

---
+++ laravel-crud/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Starting with the name of Alla. Who is most kind an merciful</h3>
+    <h4>Hey we are comming from home</h4>
+</body>
+</html>
